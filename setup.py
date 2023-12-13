f57ad125612952d0fb94cd3d3ca361c40f5f3480
Constantine Evans <const@costi.net>
2023-12-13 19:27:25 +0000

    update version reqs:
    - stickydesign 0.9.0.a2 is now compatible (reimplements fastsub in rust).
    - peppercompiler 0.1.4 (not yet released) fixes several file name problems

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='piperine',
     version='0.4a',
 
     packages=['piperine', 'piperine.tests', 'piperine.Srinivas2017', 'piperine.Chen2013'],
-    install_requires=["numpy","scipy", "stickydesign <0.9,>=0.8.4", "peppercompiler"],
+    install_requires=["numpy","scipy","stickydesign >=0.8.4,!=0.9.0.a1","peppercompiler >= 0.1.4"],
     include_package_data=True,
 
     package_data={
